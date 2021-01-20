2c05dc2c58637a5797ea17cba6b8189a39bf375a
Kilian Persichini <kpersich@e3r5p6.42roma.it>
2021-01-20 18:25:54 +0100

    piscina

---
+++ piscine/c03 main/ex04/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpersich <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/07 09:34:20 by kpersich          #+#    #+#             */
+/*   Updated: 2020/12/09 11:20:37 by kpersich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+char *ft_strstr(char *str, char *to_find);
+
+int main(void)
+{
+	char str[] = "ciaaaaaaaaaiaoao";
+	char to_find[] = "iao";
+
+	ft_strstr(str, to_find);
+
+	printf("%s\n", ft_strstr(str, to_find));
+}
---
+++ piscine/split/ft_split.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpersich <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/16 19:04:20 by kpersich          #+#    #+#             */
+/*   Updated: 2021/01/18 10:20:23 by kpersich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int  ft_word(const char *s, char c)
+{
+    size_t i;
+    size_t k;
+
+    i = 0;
+    k = 0;
+    while (s[i])
+    {
+        if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
+            k++;
+        i++;
+    }
+    return (k);
+}
+
+char        **ft_split(char const *s, char c)
+{
+    char    **a;
+    size_t  i;
+    size_t  word;
+
+    i = 0;
+    word = 0;
+    if ((!s) || (!(a = (char**)ft_calloc((ft_word(s, c) + 1), sizeof(char*)))))
+        return (NULL);
+    while (*s)
+    {
+        if (*s == c)
+        {
+            s++;
+            continue ;
+        }
+        while (*s != c && *s)
+        {
+            s++;
+            i++;
+        }
+        a[word++] = ft_substr(s - i, 0, i);
+        i = 0;
+    }
+    return (a);
+}
