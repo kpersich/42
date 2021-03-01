c8371eac9c811687148afc42d30d0bd383428b81
kpersich <kilian.persichini@hotmail.it>
2021-03-01 10:33:20 +0100

    rank 02 no printf

---
+++ exam 02/gnl/get_next_line.c
@@ -0,0 +1,50 @@
+#include "get_next_line.h"
+
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int get_next_line(char **line)
+{
+	int i;
+	int k;
+	int red;
+	char buffer[1024*1024];
+
+	if (!line)
+		return (-1);
+	i = 0;
+	k = 0;
+	while (i < (1024*1024))
+	{
+		buffer[i] = 0;
+		i++;
+	}
+	while ((red = read(0, &buffer[k], 1)) >= 0)
+	{
+		if (buffer[k] == '\n' || buffer[k] == '\0')
+		{
+			buffer[k] = 0;
+			*line = malloc(ft_strlen(buffer) + 1);
+			if (!(*line))
+				return (-1);
+			i = 0;
+			while (i < k)
+			{
+				(*line)[i] = buffer[i];
+				i++;
+			}
+			(*line)[i] = 0;
+			return (red == 0 ? 0 : 1);
+		}
+		else
+			k++;
+	}
+	return (-1);
+}
---
+++ exam 02/inter/inter6.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+	int i;
+	int k;
+	char used[255];
+
+	if (argc == 3)
+	{
+		i = 0;
+		while (i < 255)
+		{
+			used[i] = 0;
+			i++;
+		}
+		i = 2;
+		while (i > 0)
+		{
+			k = 0;
+			while (argv[i][k])
+			{
+				if (i == 2 && !used[(unsigned char)argv[i][k]])
+					used[(unsigned char)argv[i][k]] = 1;
+				if (i == 1 && used[(unsigned char)argv[i][k]])
+				{
+					used[(unsigned char)argv[i][k]] = 2;
+					write(1, &argv[i][k], 1);
+				}
+				k++;
+			}
+			i--;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
